4f9e4ac9a64186a9f098e8313f354aff241077e9
Ranjit Rupnawar <162142856+ranjitrupnawar@users.noreply.github.com>
2024-08-12 11:47:55 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       watch: true,
       // Define a health check
       health_check: {
-        url: 'http://localhost:3000/health', // Example endpoint
+        url: 'http://54.255.202.15:8080:3000/health', // Example endpoint
         interval: 30000, // Check every 30 seconds
         timeout: 5000, // Timeout after 5 seconds
         retries: 3, // Retry 3 times before considering it failed

723b709d1d918d94546f5a5ff31a2accb99df60a
Ranjit Rupnawar <162142856+ranjitrupnawar@users.noreply.github.com>
2024-08-12 10:33:54 +0530

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'node-webhook',
+      script: 'dist/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: true,
+      // Define a health check
+      health_check: {
+        url: 'http://localhost:3000/health', // Example endpoint
+        interval: 30000, // Check every 30 seconds
+        timeout: 5000, // Timeout after 5 seconds
+        retries: 3, // Retry 3 times before considering it failed
+      },
+    },
+  ],
+};
